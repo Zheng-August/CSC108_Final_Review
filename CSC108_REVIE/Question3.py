6a08d97d7290cb587e64730eecf3cafd047ddd13
Zheng-August <117753627+Zheng-August@users.noreply.github.com>
2023-12-02 22:03:52 -0500

    Add files via upload

---
+++ CSC108_REVIE/Question3.py
@@ -0,0 +1,32 @@
+'''
+Complete this function according to its docstring by implementing the algorithm below:
+    1. Use an integer accumulator to keep track of the number of matches.
+    2. For each item in lst, compare it to all other items in lst and if the item matches an item other than itself, add to
+    the number of matches.
+    3. Return the number of duplicates.
+Note: you must not use any list methods, but you may use the built-in function len().
+To earn marks for this question, you must follow the algorithm described above.
+
+'''
+
+def count_duplicates_v1(lst: list[int]) -> int:
+    """Return the number of duplicates in lst.
+    Precondition: each item in lst occurs either once or twice.
+    >>> count_duplicates_v1([1, 2, 3, 4])
+    0
+    >>> count_duplicates_v1([2, 4, 3, 3, 1, 4])
+    2
+    """
+    # Your code here
+    matches = []
+    for i in range(len(lst)-1,-1,-1):
+        element = lst[i]
+        lst.pop()
+        if not element in matches and element in lst:
+            matches.append(element)
+    return len(matches)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
